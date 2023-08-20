d982cf3d33d2e94508df92e7b6e80d6aa90ea319
NEM-ICU <itt1819025@tec.rjt.ac.lk>
2023-08-20 13:57:55 +0530

    create contact and feedback pages

---
+++ src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PrivacyPolicyComp from '../components/PrivacyPolicyComp'
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <PrivacyPolicyComp />
+    </div>
+  )
+}
+
+export default PrivacyPolicy
