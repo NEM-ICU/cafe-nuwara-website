fc4045518973503ba47cb954fbb396a6108ca89f
NEM-ICU <itt1819025@tec.rjt.ac.lk>
2023-08-28 01:31:00 +0530

    working color changing feature

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,51 @@ import ContactUs from "./pages/ContactUs";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import UnderConstruction from "./pages/UnderConstruction";
 
+// color changin feature
+import { useEffect } from "react";
+
 function App() {
+  // useEffect(() => {
+  //   const images = document.querySelectorAll("img");
+  //   const observer = new IntersectionObserver((entries) => {
+  //     entries.forEach((entry) => {
+  //       console.log(entry);
+  //     });
+  //   });
+
+  //   images.forEach((image) => {
+  //     observer.observe(image);
+  //   });
+  // }, []);
+
+  useEffect(() => {
+    const images = document.querySelectorAll("img");
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const image = entry.target;
+
+        if (entry.isIntersecting) {
+          // Add grayscale filter when the image is in view
+          image.style.filter = "none";
+        } else {
+          // Remove grayscale filter when the image is out of view
+          // image.style.filter = "none";
+        }
+      });
+    });
+
+    images.forEach((image) => {
+      observer.observe(image);
+    });
+
+    // Cleanup the observer when the component unmounts
+    return () => {
+      images.forEach((image) => {
+        observer.unobserve(image);
+      });
+    };
+  }, []);
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route
